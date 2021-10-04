df480a74ab74ee03d7f21efa6dbc6b1b408624e1
athreyadattag <67873006+Athreyadg@users.noreply.github.com>
2021-10-04 18:00:49 +0530

    Replaced variables in Firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,18 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey:"AIzaSyA_lhkL2XGtSWlaRU9WN4IVWAdc39TBugg",
+//   process.env.REACT_APP_FIREBASE_KEY,
+  authDomain:"shopping-cart-705aa.firebaseapp.com",
+//   process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId:"shopping-cart-705aa",
+//   process.env.REACT_APP_FIREBASE_PROJECT_ID,
   databaseURL: "https://shopping-cart-705aa-default-rtdb.firebaseio.com",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+
+  storageBucket:"shopping-cart-705aa.appspot.com",
+//   process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:"680389323882",
+//   process.env.REACT_APP_FIREBASE_SENDER_ID,
 });
 
 export const auth=app.auth()
